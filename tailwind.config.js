acaa985f38ce6026694f32e9998f355a5e213589
Sheikh Farid <mdfaridsagor101@gmail.com>
2024-08-21 10:48:51 +0600

    added nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ['*'],
   theme: {
     extend: {
+      colors: {
+        'primary-color': '#B68C5A',
+      },
       fontFamily: {
         'poppins': ["Poppins", 'sans-serif']
       },
